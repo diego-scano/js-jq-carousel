d2a71295ef6867d6d22094ba0d99da2b80c77c7a
Diego <diego.scano77@gmail.com>
2021-01-13 16:54:58 +0100

    es completo

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-var last = document.getElementsByClassName('last');
-
 $('.next i').click(function () {
-  $('.images > .active').removeClass('active').next().addClass('active');
-  $('.nav > i.active').removeClass('active').next().addClass('active');
+
+  var currentImg = $('.images img.active');
+  currentImg.removeClass('active');
+
+  if(currentImg.hasClass('last')) {
+    var firstImg = $('.images img.first');
+    firstImg.addClass('active');
+  } else {
+    currentImg.removeClass('active').next().addClass('active');
+  }
+
+  var currentCircle = $('.nav i.active');
+  currentCircle.removeClass('active');
+
+  if(currentCircle.hasClass('last')) {
+    var firstCircle = $('.nav i.first');
+    firstCircle.addClass('active');
+  } else {
+    currentCircle.removeClass('active').next().addClass('active');
+  }
 })
 
+
 $('.prev i').click(function () {
-  $('.images > .active').removeClass('active').prev().addClass('active');
-  $('.nav > i.active').removeClass('active').prev().addClass('active');
+
+  var currentImg = $('.images img.active');
+  currentImg.removeClass('active');
+
+  if(currentImg.hasClass('first')) {
+    var lastImg = $('.images img.last');
+    lastImg.addClass('active');
+  } else {
+    currentImg.removeClass('active').prev().addClass('active');
+  }
+
+  var currentCircle = $('.nav i.active');
+  currentCircle.removeClass('active');
+
+  if(currentCircle.hasClass('first')) {
+    var lastCircle = $('.nav i.last');
+    lastCircle.addClass('active');
+  } else {
+    currentCircle.removeClass('active').prev().addClass('active');
+  }
 })
